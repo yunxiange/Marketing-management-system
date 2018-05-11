afd3c71690f212c39b7222a922e8f28e6d64cd2e
Comet8023 <liuwenhan.comet@qq.com>
2018-05-11 23:47:33 +0800

    test

--- src/routes/SaleManagement/SaleDataInputPage.js
+++ src/routes/SaleManagement/SaleDataInputPage.js
@@ -90,7 +90,7 @@ export default class SaleDataInputPage extends PureComponent {
 
         return (
           <PageHeaderLayout>
-            
+            <h1>Hello World</h1>
         </PageHeaderLayout>
         );
       }
